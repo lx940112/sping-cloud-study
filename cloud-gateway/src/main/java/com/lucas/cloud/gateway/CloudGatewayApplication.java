4ee3a5c4aafc3faf59d24a8833f65523878f8c6f
刘旭 <754352606@qq.com>
2019-07-03 11:20:23 +0800

    commit

---
+++ cloud-gateway/src/main/java/com/lucas/cloud/gateway/CloudGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.lucas.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lucas
+ */
+@SpringBootApplication
+public class CloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayApplication.class, args);
+    }
+
+}
