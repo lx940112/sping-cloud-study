1ffe3be642e56583a1cb297409172f4e2efbfb68
刘旭 <754352606@qq.com>
2019-09-03 18:40:53 +0800

    docker demo commit

---
+++ docker-demo/src/main/java/com/lucas/docker/demo/DockerDemoApplication.java
@@ -0,0 +1,16 @@
+package com.lucas.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lucas
+ */
+@SpringBootApplication
+public class DockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
+
+}
